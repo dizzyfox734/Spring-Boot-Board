07d2dfc4170a5f360ecb848f9863d6717868b0cb
dizzyfox734 <dizzyfox734@gmail.com>
2023-05-22 00:48:58 +0900

    feat : 댓글 수정 기능 추가

--- src/main/java/dizzyfox734/springbootboard/controller/CommentController.java
+++ src/main/java/dizzyfox734/springbootboard/controller/CommentController.java
@@ -1,6 +1,7 @@
 package dizzyfox734.springbootboard.controller;
 
 import dizzyfox734.springbootboard.controller.dto.CommentDto;
+import dizzyfox734.springbootboard.domain.comment.Comment;
 import dizzyfox734.springbootboard.domain.post.Post;
 import dizzyfox734.springbootboard.domain.user.User;
 import dizzyfox734.springbootboard.service.CommentService;
@@ -8,13 +9,16 @@ import dizzyfox734.springbootboard.service.PostService;
 import dizzyfox734.springbootboard.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -43,5 +47,36 @@ public class CommentController {
         }
         this.commentService.create(post, commentDto.getContent(), user);
         return String.format("redirect:/post/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modify(CommentDto commentDto, @PathVariable("id") Integer id, Principal principal) {
+        Comment comment = this.commentService.findOne(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentDto.setContent(comment.getContent());
+        return "comment/form";
+    }
+
+    /**
+     * 댓글 수정
+     */
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modify(@Valid CommentDto commentDto, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "comment/form";
+        }
+        Comment comment = this.commentService.findOne(id);
+
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.commentService.modify(comment, commentDto.getContent());
+
+        return String.format("redirect:/post/detail/%s", comment.getPost().getId());
     }
 }
--- src/main/java/dizzyfox734/springbootboard/service/CommentService.java
+++ src/main/java/dizzyfox734/springbootboard/service/CommentService.java
@@ -4,8 +4,11 @@ import dizzyfox734.springbootboard.domain.comment.Comment;
 import dizzyfox734.springbootboard.domain.post.Post;
 import dizzyfox734.springbootboard.domain.comment.CommentRepository;
 import dizzyfox734.springbootboard.domain.user.User;
+import dizzyfox734.springbootboard.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +22,21 @@ public class CommentService {
         comment.setContent(content);
         comment.setPost(post);
         comment.setAuthor(author);
+        this.commentRepository.save(comment);
+    }
+
+    public Comment findOne(Integer id) {
+        Optional<Comment> comment = this.commentRepository.findById(id);
+
+        if (comment.isPresent()) {
+            return comment.get();
+        } else {
+            throw new DataNotFoundException("comment not found");
+        }
+    }
+
+    public void modify(Comment comment, String content) {
+        comment.setContent(content);
         this.commentRepository.save(comment);
     }
 }
