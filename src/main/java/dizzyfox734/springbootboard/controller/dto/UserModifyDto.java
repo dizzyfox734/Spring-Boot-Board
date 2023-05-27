449ad05d12574514f40155c4d5ea74a931eb70fd
dizzyfox734 <dizzyfox734@gmail.com>
2023-05-27 21:33:16 +0900

    feat : 유저 정보 수정 기능 추가 (완료)
    - UserController에 유저 정보 수정 함수 생성
    - 유저 정보 수정을 위한 UserModifyDto 생성
    - UserService에 유저 정보 변경 함수 생성
    - 유저 정보 페이지 수정

---
+++ src/main/java/dizzyfox734/springbootboard/controller/dto/UserModifyDto.java
@@ -0,0 +1,16 @@
+package dizzyfox734.springbootboard.controller.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserUpdateDto {
+
+    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    private String password1;
+
+    @NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+    private String password2;
+}
