2b18e65b86b8f93f5263905d2cc5b7422657ba37
dizzyfox734 <dizzyfox734@gmail.com>
2023-11-27 21:18:47 +0900

    chore : 데이터베이스 RDS로 이동
    - spring boot 설정파일 수정
    - docker mysql 관련 코드, 환경변수 제거

--- docker/scripts/destroy.sh
+++ docker/scripts/destroy.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker-compose down --volumes
-docker rmi spring-boot-board_web spring-boot-board_db spring-boot-board_redis #docker-test-nginx
+docker rmi spring-boot-board_web spring-boot-board_redis #docker-test-nginx
--- web/spring-boot-board/src/main/java/dizzyfox734/springbootboard/domain/user/User.java
+++ web/spring-boot-board/src/main/java/dizzyfox734/springbootboard/domain/user/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "`user`")
+@Table(name = "users")
 @Getter
 @Setter
 @Builder
