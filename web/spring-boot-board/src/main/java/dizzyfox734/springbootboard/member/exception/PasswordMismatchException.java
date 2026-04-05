af99863a9e215e2092645f31902978c9cbdc9919
dizzyfox734 <dizzyfox734@gmail.com>
2026-04-05 19:58:06 +0900

    refact : 사용되지 않는 예외 클래스 삭제
    - MailAuthenticationFailedException 클래스 제거
    - NotFoundUserException 클래스 제거
    - PasswordMismatchException 클래스 제거
    - 미사용 코드 정리로 코드베이스 단순화 및 유지보수성 개선

--- web/spring-boot-board/src/main/java/dizzyfox734/springbootboard/member/exception/PasswordMismatchException.java
+++
@@ -1,9 +0,0 @@
-package dizzyfox734.springbootboard.member.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
